46efc5174f13f17d943d57834a0982ba52e3d1d5
Regorlas <roger.random@outlook.com>
2022-12-25 00:58:00 -0800

    Blocking test to read chip ID

--- nonblock_onramp/nonblock_onramp.ino
+++ nonblock_onramp/nonblock_onramp.ino
@@ -18,6 +18,7 @@
 #include <mozzi_midi.h>
 #include <Oscil.h>  // a template for an oscillator
 #include <tables/sin2048_int8.h>  // a wavetable holding a sine wave
+#include <twi_nonblock.h>
 
 // I expected these to be in Mozzi library somewhere but I failed to find it.
 const int note_C4 = mtof(60);
@@ -31,31 +32,114 @@ const int note_C5 = mtof(72);
 
 int scale4[] = {note_C4, note_D4, note_E4, note_F4, note_G4, note_A4, note_B4, note_C5};
 
-#define CONTROL_RATE 128
+// Mozzi setup
 Oscil <2048, AUDIO_RATE> aSin(SIN2048_DATA);
 
+// Tracking what note we are playing and when to update
 unsigned long nextUpdate;
 uint8_t currentNote;
 
+// AS7341 constants copied from Adafruit_AS7341.h
+#define AS7341_I2CADDR_DEFAULT 0x39 ///< AS7341 default i2c address
+#define AS7341_CHIP_ID 0x09         ///< AS7341 default device id from WHOAMI
+#define AS7341_REVID 0x91           ///< Chip revision register
+#define AS7341_WHOAMI 0x92          ///< Chip ID register
+
+// Utility functions calling into Mozzi twi_nonblock
+#define TWI_IDLE 0
+#define TWI_READING 1
+#define TWI_WRITING 2
+
+void twi_readStart(uint8_t i2c_address, uint8_t register_id, uint8_t length) {
+  uint8_t retVal = 0;
+
+  txBuffer[0] = register_id;
+  txBufferIndex = 1;
+  txBufferLength = 1;
+  twi_initiateWriteTo(i2c_address, txBuffer, txBufferLength);
+
+  Serial.println("Write I2C register address");
+  twi_state = TWI_WRITING;
+  while (TWI_WRITING == twi_state) {
+    if ( TWI_MTX != twi_state ){
+      txBufferIndex = 0;
+      txBufferLength = 0;
+
+      retVal = twi_initiateReadFrom(i2c_address, length);
+
+      if (retVal != 0) {
+        Serial.print("twi_initiateReadFrom failed with ");
+        Serial.println(retVal);
+        // Reset to idle in case of failure
+        twi_state = TWI_IDLE;
+      } else {
+        twi_state = TWI_READING;
+      }
+    }
+  }
+  Serial.println("Receive data");
+  while (TWI_READING == twi_state) {
+    if (TWI_MRX != twi_state) {
+      uint8_t readBuffer[32];
+      uint8_t available = twi_readMasterBuffer(readBuffer, length);
+
+      Serial.print("Available bytes: ");
+      Serial.println(available);
+
+      for (int i = 0; i < available; i++) {
+        Serial.println(readBuffer[i]);
+      }
+
+      Serial.println("Read operation complete");
+      twi_state = TWI_IDLE;
+    }
+  }
+}
+
+// AS7341 code
+void as7341_setup() {
+  uint8_t chip_id;
+  uint8_t bytesRead = 0;
+
+  Serial.print("twi_state = ");
+  Serial.println(twi_state);
+  Serial.print("twi_error = ");
+  Serial.println(twi_error);
+
+  twi_readStart(AS7341_I2CADDR_DEFAULT, AS7341_REVID, 2);
+
+  if (1 != bytesRead) {
+    Serial.print("Expected to read single byte for ID but read ");
+    Serial.println(bytesRead);
+  }
+}
+
+// Arduino boilerplate onetime setup code
 void setup() {
   Serial.begin(115200);
   // Wait for communication with the host computer serial monitor
   while (!Serial) {
     delay(1);
   }
+  Serial.println("Serial online");
 
 	aSin.setFreq(440);
 	startMozzi(CONTROL_RATE);
   nextUpdate = millis();
   currentNote = 0;
-  Serial.println("setup() complete");
+  Serial.println("Mozzi online");
+
+  initialize_twi_nonblock();
+  Serial.println("twi_nonblock initialized");
+
+  as7341_setup();
+  Serial.println("AS7341 initialized");
+
+  Serial.println("Setup complete");
 }
 
 void updateControl(){
   if (millis() > nextUpdate) {
-    Serial.print("Changing note to ");
-    Serial.println(currentNote);
-
     aSin.setFreq(scale4[currentNote++]);
     currentNote = currentNote % 8;
     nextUpdate = millis() + 500;
