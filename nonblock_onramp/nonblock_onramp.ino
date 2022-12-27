5b040a5c88f8f8dff6a988d8c2e0724e9de07bf4
Regorlas <roger.random@outlook.com>
2022-12-26 23:27:16 -0800

    MIT License header

--- nonblock_onramp/nonblock_onramp.ino
+++ nonblock_onramp/nonblock_onramp.ino
@@ -1,4 +1,6 @@
-/* Experiments building onramp to use AMS AS7341 sensor via twi_nonblock I2C.
+/*
+ * Mozzi example using AMS AS7341 sensor via twi_nonblock I2C.
+ *
  * Adafruit's sample code for AS7341 sensor uses Arduino's Wire library for
  * I2C communication.
  * https://github.com/adafruit/Adafruit_AS7341
@@ -18,6 +20,28 @@
  * Hardware: Mozzi twi_nonblock only supports AVR microcontrollers including
  * the ATMega328P used in Arduino Nano. This will not run on non-AVR chips
  * like the ESP32.
+ *
+ * Released under MIT License:
+
+  Copyright (c) 2022 Roger Cheng
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
  */
 
 #include <MozziGuts.h>   // at the top of your sketch
